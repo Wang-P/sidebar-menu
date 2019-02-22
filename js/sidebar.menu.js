4a1168a2334e4b23193565bac57208fc669ac12d
PHP <PHP@192.168.0.171>
2019-02-22 13:57:19 +0100

    dev

--- js/sidebar.menu.js
+++ js/sidebar.menu.js
@@ -15,7 +15,7 @@
 	$('ul.menu li').each(function() {
 		$(this).parent().find('li.parent a.employ').addClass('up');
 		if ($(this).children('li.parent a.current').length > 0 ) {
-			$(this).parent().find('ul.submenu').toggle();
+			$(this).parents().find('a.current').next('ul.submenu').show();
 			$(this).parent().find('li.parent a.employ').addClass('active down');
 		}
 	});
@@ -23,8 +23,7 @@
 	// active menu
 	$('ul.menu li a').on('click', function() {
 		$(this).parent('li.parent').find('a.employ').toggleClass('active');
-		$(this).parent().find('ul.submenu').slideToggle('fast');
-		$(this).parent().find('ul.submenu ul.submenu').toggle();
+		$(this).next('ul.submenu').slideToggle('fast');
 	});
 
 	// click transition
