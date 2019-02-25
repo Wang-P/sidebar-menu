31cc908e9eca07dbf1ed86ca888697efbf59df0b
PHP <PHP@192.168.0.171>
2019-02-25 20:55:33 +0100

    dev

--- js/sidebar.menu_dev.js
+++ js/sidebar.menu_dev.js
@@ -14,15 +14,15 @@
 	// list init
 	$('.list-ml-item').each(function() {
 		$(this).parent().find('.link-arrow').addClass('up');
-		if ($(this).children('.current').length > 0 ) {
-			$(this).parents().find('.current').next('.list-ml-none').show();
-			$(this).parent().find('.link-arrow').addClass('a down');
+		if ($(this).children('.list-link-current').length > 0 ) {
+			$(this).parents().find('.list-link-current').next('.list-ml-none').show();
+			$(this).parent().find('.link-arrow').addClass('active down');
 		}
 	});
 
 	// list open hidden
 	$('.list-link').on('click', function() {
-		//$(this).parent().find('.link-arrow').toggleClass('b-c');
+		$(this).parent().find('.link-arrow').toggleClass('active');
 		$(this).next('.list-ml-none').slideToggle('fast');
 	});
 
@@ -30,6 +30,9 @@
 	$('.link-arrow').on('click', function() {
 		$(this).addClass('transition');
 		$(this).toggleClass('rotate');
+		if ($(this).parent().find('.link-arrow').hasClass('down')) {
+			$(this).toggleClass('rotate-revert');
+		}
 	});
 
 }(jQuery));
