fcbc07dd6d364950c860f6805a28c183ff49c267
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-05-06 08:46:05 +0200

    Update sidebar.menu.js

--- js/sidebar.menu.js
+++ js/sidebar.menu.js
@@ -5,6 +5,12 @@
  */
 (function($) {
 
+  // class with arrow icon
+  const linkArrow   = '.link-arrow';
+
+  // class to hide the list
+  const listHidden  = '.list-hidden';
+
   // toggle sidebar menu
   $('#sidebar-toggle').on('click', function() {
     $('#wrapper').toggleClass('sidebar-toggle');
@@ -12,25 +18,49 @@
 
   // list init
   $('.list-item').each(function() {
-    $(this).parent().find('.link-arrow').addClass('up');
-    if ($(this).find('.link-current').length > 0) {
-        $(this).parent().find('.link-current.link-arrow').addClass('active down');
-        $(this).parent().find('.link-current').next('.list-hidden').show();
+
+    const el = $(this);
+    const parent  = el.parent();
+
+    // current link class
+    const current = parent.find('.link-current');
+
+    // class active link with icon
+    const active  = current.addClass('active down');
+
+    // change up arrow icon
+    parent.find(linkArrow).addClass('up');
+
+    // view the list above the current link
+    if (current.length > 0) {
+      active.next(listHidden).show();
     }
   });
 
   // list open hidden
   $('.list-link').on('click', function() {
-    $(this).parent().find('.link-arrow').toggleClass('active');
-    $(this).next('.list-hidden').slideToggle('fast');
+
+    const el = $(this);
+
+    // switch the class on the current link
+    el.parent().find(linkArrow).toggleClass('active');
+
+    // view a hidden list
+    el.next(listHidden).slideToggle('fast');
+
   });
 
   // list transition arrow
   $('.link-arrow').on('click', function() {
-    $(this).addClass('transition');
-    $(this).toggleClass('rotate');
-    if ($(this).parent().find('.link-arrow').hasClass('down')) {
-        $(this).toggleClass('rotate-revert');
+
+    const el = $(this);
+
+    // adding rotation effect to arrows icons
+    el.addClass('transition').toggleClass('rotate');
+
+    // rotate the direction of rotation of the arrow
+    if (el.parent().find(linkArrow).hasClass('down')) {
+      el.toggleClass('rotate-revert');
     }
   });
 
